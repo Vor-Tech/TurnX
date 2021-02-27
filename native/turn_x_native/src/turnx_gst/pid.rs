11914f7c05c8c9f2cbdc3f2e05654e27085802f3
Roland Metivier <metivier.roland@chlorophyt.us>
2021-02-27 01:12:37 -0600

    add f64 case of PID controller

--- native/turn_x_native/src/turnx_gst/pid.rs
+++ native/turn_x_native/src/turnx_gst/pid.rs
@@ -98,6 +98,76 @@ impl PIDController<f32> {
     }
 }
 
+impl PIDController<f64> {
+    pub fn new(
+        min: f64,
+        max: f64,
+        weight_proportion: f64,
+        weight_integral: f64,
+        weight_derivative: f64,
+        integral_size: usize,
+        start_at: f64,
+        start_proportion: f64,
+        start_derivative: f64,
+    ) -> PIDController<f64> {
+        PIDController {
+            proportion: start_proportion,
+            integral: vec![0.0_f64; integral_size].into_iter().collect(),
+            integral_size: integral_size,
+            derivative: start_derivative,
+            derivative_previous: start_derivative,
+            current: start_at,
+            weight_proportion: weight_proportion,
+            weight_integral: weight_integral,
+            weight_derivative: weight_derivative,
+            min: min,
+            max: max,
+        }
+    }
+
+    pub fn adjust(&mut self, value: f64) -> f64 {
+        // adjust proportion here
+        self.proportion = value - self.current;
+
+        // adjust integral here
+        while self.integral_size < self.integral.len() {
+            // the integral is too large, pop desired elems
+            self.integral.pop_back();
+        }
+        while (self.integral_size - 1) > self.integral.len() {
+            // the integral is too small, repeatedly zero
+            self.integral.push_front(0.0_f64);
+        }
+        // finally the integral is good, push in a zero
+        self.integral.push_front(self.proportion);
+
+        // adjust derivative here
+        let derivative_hold = self.derivative_previous;
+        self.derivative_previous = self.derivative;
+        self.derivative = (self.proportion - derivative_hold);
+
+        // calc result
+        let result: f64 = (self.proportion * self.weight_proportion)
+            + ((self.integral.clone().into_iter().sum::<f64>() / (self.integral.len() as f64))
+                * self.weight_integral)
+            + (self.derivative * self.weight_derivative);
+        assert!(
+            !result.is_nan(),
+            "PID control resulted in abnormal NaN delta"
+        );
+
+        // set current then clamp
+        self.current = self.current + result;
+        assert!(
+            !self.current.is_nan(),
+            "PID control resulted in abnormal NaN current value"
+        );
+        self.current = self.current.clamp(self.min, self.max);
+
+        self.current
+    }
+}
+
 // ============================================================================
 // TEST
 // ============================================================================
